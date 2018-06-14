8a5e8b34f64157b041512ff2fcde37887a50213a
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-06-14 14:49:27 +0100

    Remove unused classNames for Count component

--- src/components/Count/index.js
+++ src/components/Count/index.js
@@ -3,20 +3,17 @@ import PropTypes from 'prop-types';
 
 const Count = (props) => {
   const {
-    className = 'result-count',
-    textClass = 'result-count-text',
     value,
   } = props;
 
   return (
-    <div className={`count ${className}`}>
-      <span className={textClass}>{value}</span>
+    <div>
+      <span>{value}</span>
     </div>
   );
 };
 
 Count.propTypes = {
-  className: PropTypes.string,
   textClass: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
