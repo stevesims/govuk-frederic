b2275f2b87d4312235fbb22c5e465d80c0f54756
Gavin Orland <gavin@gavinorland.com>
2018-06-15 11:26:11 +0100

    Set min-height rule on TextArea in CountdownTextarea

--- src/components/CountdownTextarea/index.js
+++ src/components/CountdownTextarea/index.js
@@ -4,6 +4,10 @@ import styled from 'react-emotion';
 import { TextAreaField } from '@govuk-react/text-area';
 import { ERROR_COLOUR } from 'govuk-colours';
 
+const StyledTextAreaField = styled(TextAreaField)({
+  minHeight: '250px',
+});
+
 const StyledTextArea = styled('div')(
   {
     display: 'flex',
@@ -33,30 +37,20 @@ const StyledCountdown = styled('div')({
 StyledCountdown.displayName = 'StyledCountdown';
 
 const CountdownTextarea = props => {
-  const {
-    maxLength,
-    noMaxLengthAttr,
-    positiveOnly,
-    ...input
-  } = props;
+  const { maxLength, noMaxLengthAttr, positiveOnly, ...input } = props;
 
   const showRemaining = !!maxLength;
   const remaining = showRemaining ? maxLength - (input.value || '').length : false;
-  const over = showRemaining && (remaining <= 0);
+  const over = showRemaining && remaining <= 0;
 
   return (
     <StyledTextArea over={over}>
-      <TextAreaField
-        {...input}
-        maxLength={noMaxLengthAttr ? null : maxLength}
-      />
-      {showRemaining &&
+      <StyledTextAreaField {...input} maxLength={noMaxLengthAttr ? null : maxLength} />
+      {showRemaining && (
         <StyledCountdownContainer>
-          <StyledCountdown>
-            {over && positiveOnly ? '0' : remaining}
-          </StyledCountdown>
+          <StyledCountdown>{over && positiveOnly ? '0' : remaining}</StyledCountdown>
         </StyledCountdownContainer>
-      }
+      )}
     </StyledTextArea>
   );
 };
