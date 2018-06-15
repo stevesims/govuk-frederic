9901dc773bb5d991455d02988bf4b079ee95d599
Gavin Orland <gavin@gavinorland.com>
2018-06-15 14:53:54 +0100

    HeaderButton - include fontFamily

--- src/components/HeaderButton/index.js
+++ src/components/HeaderButton/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
 
-import { FOCUS_COLOUR, LINK_ACTIVE_COLOUR, LINK_COLOUR} from 'govuk-colours';
+import { FOCUS_COLOUR, LINK_ACTIVE_COLOUR, LINK_COLOUR } from 'govuk-colours';
 
 const Button = glamorous.button(
   {
@@ -12,36 +12,39 @@ const Button = glamorous.button(
     borderRight: 'none',
     borderTop: '4px solid transparent',
     display: 'inline-block',
+    fontFamily: 'Roboto, Arial, sans-serif',
     fontSize: '18px',
     fontWeight: 'bold',
     minWidth: '190px',
     padding: '8px',
     textAlign: 'center',
   },
-  ({ active }) => (
-    active ? {
-      borderBottomColor: LINK_COLOUR,
-      borderTopColor: LINK_COLOUR,
-      color: LINK_COLOUR,
-    } : {}
-  ),
-  ({ disabled }) => (
-    disabled ? {
-      opacity: '0.35',
-    } : {
-      ':focus': {
-        background: `${FOCUS_COLOUR}`,
-        outline: `1px solid ${FOCUS_COLOUR}`,
+  ({ active }) =>
+    active
+      ? {
+        borderBottomColor: LINK_COLOUR,
+        borderTopColor: LINK_COLOUR,
+        color: LINK_COLOUR,
+      }
+      : {},
+  ({ disabled }) =>
+    disabled
+      ? {
+        opacity: '0.35',
+      }
+      : {
+        ':focus': {
+          background: `${FOCUS_COLOUR}`,
+          outline: `1px solid ${FOCUS_COLOUR}`,
+        },
+        ':hover': {
+          borderBottomColor: `${LINK_ACTIVE_COLOUR}`,
+          borderTopColor: `${LINK_ACTIVE_COLOUR}`,
+        },
       },
-      ':hover': {
-        borderBottomColor: `${LINK_ACTIVE_COLOUR}`,
-        borderTopColor: `${LINK_ACTIVE_COLOUR}`,
-      },
-    }
-  ),
 );
 
-const HeaderButton = ({active, children, disabled, ...props}) => (
+const HeaderButton = ({ active, children, disabled, ...props }) => (
   <Button {...props} active={active} disabled={disabled}>
     {children}
   </Button>
