3e7101049fc0e337724420ffeeb43210e7b2000c
Gavin Orland <gavin@gavinorland.com>
2018-06-15 15:03:14 +0100

    Keyline - Add fontFamily

--- src/components/Keyline/index.js
+++ src/components/Keyline/index.js
@@ -4,16 +4,14 @@ import glamorous from 'glamorous';
 
 import { GREY_1 } from 'govuk-colours';
 
-const KeylineWrapper = glamorous.span(({
-  inlineBlock,
-  borderColor,
-}) => ({
+const KeylineWrapper = glamorous.span(({ inlineBlock, borderColor }) => ({
   border: 0,
   borderBottom: `1px solid ${borderColor ? borderColor : GREY_1}`,
   display: inlineBlock ? 'inline-block' : 'block',
+  fontFamily: 'Roboto, Arial, sans-serif',
 }));
 
-const Keyline = ({ children, inlineBlock, borderColor}) => (
+const Keyline = ({ children, inlineBlock, borderColor }) => (
   <KeylineWrapper inlineBlock={inlineBlock} borderColor={borderColor}>
     {children}
   </KeylineWrapper>
