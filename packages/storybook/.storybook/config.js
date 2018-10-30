02e1d674ed3c07201c38347f2862bc13ea1767f1
Gavin Orland <gavin.orland@gmail.com>
2018-10-30 16:16:33 +0000

    Use ReadMeHidePreview from govuk-react (#81)

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,5 +1,11 @@
 import { configure } from '@storybook/react';
 import 'react-chromatic/storybook-addon';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: 'govuk-frederic',
+  sortStoriesByKind: true,
+});
 
 function loadStories() {
   require('../src/stories');
